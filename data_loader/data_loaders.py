14477f9c409cb5b59f487592131d1194d5b9d451
Tomasz Dominiak <bri.t.dominiak@zabka.pl>
2023-10-28 23:10:25 +0200

    add base template

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,27 @@
-from torchvision import datasets, transforms
+from typing import Dict
+
+from torchvision import transforms
+
 from base import BaseDataLoader
+from datasets.dataset_2d import CustomDataset2D
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
+class CustomDataLoader(BaseDataLoader):
+
+    def __init__(self, dataset_args: Dict, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        # self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+    def _init_dataset(self):
+        pass
+
+
+
+
+
+
+
+
+
+
---
+++ image_preprocesing.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+# TODO(PS): add preprocessing here.
+def main():
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    main()
